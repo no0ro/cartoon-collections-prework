885d253cee075cf475f4e23e65b9f0e6f730a460
Noelle Rolwes <nrolwes@gmail.com>
2018-09-11 23:12:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index{ |item, index|
+    puts "#{index + 1} #{item}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  calls = []
+  planeteer_calls.map do |x|
+    calls << "#{x}!".capitalize
+  end
+  calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  puts false when calls.length < 4
+  range = calls.length > 4
+  calls.include?(range)
+  end 
 end
 
 def find_the_cheese# code an argument here
